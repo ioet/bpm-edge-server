1dd1b7c85568ccd0c74e357eaa11e94696b88e71
Lukas Ziefle <lziefle@ioet.com>
2018-10-01 13:58:21 -0500

    fixed cors filter. It didn't work for CREATE, PUT, DELETE

--- src/main/java/com/ioet/edgeserver/EdgeServerApplication.java
+++ src/main/java/com/ioet/edgeserver/EdgeServerApplication.java
@@ -1,11 +1,16 @@
 package com.ioet.edgeserver;
 
-import com.ioet.edgeserver.filter.CorsFilter;
-import com.ioet.edgeserver.filter.CorsRequestFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @EnableZuulProxy
 @SpringBootApplication
@@ -16,12 +21,26 @@ public class EdgeServerApplication {
     }
 
     @Bean
-    public CorsFilter simpleFilter() {
-        return new CorsFilter();
-    }
-
-    @Bean
-    public CorsRequestFilter getCorsRequestFilter() {
-        return new CorsRequestFilter();
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        List<String> allowedHeaders = new ArrayList<>();
+        allowedHeaders.add("Content-Type");
+        allowedHeaders.add("Accept");
+        config.setAllowedHeaders(allowedHeaders);
+        List<String> allowedMethods = new ArrayList<>();
+        allowedMethods.add("GET");
+        allowedMethods.add("POST");
+        allowedMethods.add("PUT");
+        allowedMethods.add("DELETE");
+        allowedMethods.add("OPTIONS");
+        allowedMethods.add("PATCH");
+        config.setAllowedMethods(allowedMethods);
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
     }
 }
